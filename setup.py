b8ac842cf62f1dd6f36e55f49351f58e9605b539
WangYG <wangyonggang@ctyun.cn>
2016-12-28 12:26:16 +0800

    change package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'py_pit',
+    name = 'pyPit',
     version = '0.1',
     keywords = ('package', 'chinatelecom'),
     description = 'package tools for packup all python source files and dependences',
